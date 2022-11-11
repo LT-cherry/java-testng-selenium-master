aa4459edbb5bde56156909548345d03bb089d2cf
blu3r0se <108914829+blu3r0se@users.noreply.github.com>
2022-11-11 15:37:30 +0530

    Update TestNGTodo3.java

--- src/test/java/com/lambdatest/TestNGTodo3.java
+++ src/test/java/com/lambdatest/TestNGTodo3.java
@@ -29,7 +29,7 @@ public class TestNGTodo3 {
         caps.setCapability("platform", "MacOS Catalina");
         caps.setCapability("browserName", "Chrome");
         caps.setCapability("version", "latest");
-        caps.setCapability("build", "TestNG With Java");
+        caps.setCapability("‘LT_BUILD_NAME", "TestNG With Java");
         caps.setCapability("name", m.getName() + this.getClass().getName());
         caps.setCapability("plugin", "git-testng");
 
